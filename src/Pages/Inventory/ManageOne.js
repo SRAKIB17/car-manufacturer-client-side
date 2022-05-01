f27af76e52b4902d62af32cd6d0846852f6ada19
SRAKIB17 <rakibulssc5@gmail.com>
2022-05-01 13:55:56 +0600

    create hooke

--- src/Pages/Inventory/ManageOne.js
+++ src/Pages/Inventory/ManageOne.js
@@ -37,7 +37,7 @@ const ManageOne = ({ id }) => {
                         </div>
                     </div>
                 </div> 
-            </div>
+            </div>  
         </div>
     );
 };
