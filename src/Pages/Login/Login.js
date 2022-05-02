ac0742017d77932e62abd068b8c8eae88829dacd
SRAKIB17 <rakibulssc5@gmail.com>
2022-05-02 17:37:51 +0600

    ok login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,7 +6,10 @@ import email from './Svg/email-svgrepo-com.svg';
 import password from './Svg/password-svgrepo-com.svg';
 import userProfile from './Svg/user-svgrepo-com.svg';
 import auth from '../../firebase.init';
-import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithFacebook, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
+import { toast } from 'react-toastify'
+
 
 const Login = () => {
     const { register, handleSubmit } = useForm();
@@ -21,69 +24,81 @@ const Login = () => {
 
     // for email passeword register 
     const [createUserWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth);
+    // for login 
+    const [signInWithEmailAndPassword, user1, loading1, error1] = useSignInWithEmailAndPassword(auth);
+    
+    // for google login 
+    const [signInWithGoogle, user2, loading2, error2] = useSignInWithGoogle(auth);
+    // for facebook Login 
+    const [signInWithFacebook, use3, loadin3, error3] = useSignInWithFacebook(auth);
 
-    const onSubmit = data => {
-         //    for signin 
-        if (!resetPass && registerUser) {
-           
-            console.log(55345534)
+    // for reset password 
+    const [sendPasswordResetEmail, sending, error4] = useSendPasswordResetEmail(auth);
+
+    const onSubmit = async data => {
+            //    for signin 
+            if (!resetPass && registerUser) {
+                await signInWithEmailAndPassword(data.email, data.password)
+                console.log(user1)
+
+            }
+            // new account 
+            else if (!resetPass && !registerUser) {
+                await createUserWithEmailAndPassword(data.email, data.password)
+                console.log(user)
+            }
+            // reset pass 
+            else if (resetPass) {
+                await sendPasswordResetEmail(data.email)
+                toast.success('send reset link on email')
+            }
         }
-        // new account 
-        else if (!resetPass && !registerUser) {
-            createUserWithEmailAndPassword(data.email,data.password)
-            console.log(user)
-        }
-        // reset pass 
-        else if (resetPass) {
-            console.log('resetpass')
-        }
-    }
     return (
-        <div>
-            <div className='loginForm'>
-                <h4>{resetPass?'Reset Password':(registerUser ? 'Login' : 'New Account')}</h4>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    {
-                        resetPass || (registerUser ||
-                            <div>
-                                <img src={userProfile} alt="" />
-                                <input type='text' {...register("firstName", { required: true, maxLength: 20 })} />
-                            </div>)
-                    }
+            <div>
+                <div className='loginForm'>
+                    <h4>{resetPass ? 'Reset Password' : (registerUser ? 'Login' : 'New Account')}</h4>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        {
+                            resetPass || (registerUser ||
+                                <div>
+                                    <img src={userProfile} alt="" />
+                                    <input type='text' {...register("firstName", { required: true, maxLength: 20 })} />
+                                </div>)
+                        }
 
-                    <div>
-                        <img src={email} alt="" />
-                        <input type='email' {...register("email", { required: true })} />
-
-                    </div>
-                    {
-                        resetPass ||
                         <div>
-                            <img src={password} alt="" />
-                            <input type="password" {...register("password", { required: true })} />
-                        </div>
-                    }
-                    <input type="submit" value={resetPass ? 'Reset Password' : (registerUser ? 'Login' : 'Register')} />
-                </form>
-                <div className='or'>
-                    <div></div>
-                    <p>Or</p>
-                    <div></div>
-                </div>
-                <div className='socialLogin'>
-                    {
-                        resetPass || <>
-                            <button><Facebook />Login with Facebook</button>
-                            <button><Google /> Login with Google</button>
-                            <button onClick={() => setResetPass(!resetPass)}>Reset Password?</button>
-                        </>
-                    }
-                    <button onClick={registerHandlar}> {registerUser ? 'Create new Account' : 'Already have account Login'}</button>
-                </div>
-            </div>
+                            <img src={email} alt="" />
+                            <input type='email' {...register("email", { required: true })} />
 
-        </div>
-    );
+                        </div>
+                        {
+                            resetPass ||
+                            <div>
+                                <img src={password} alt="" />
+                                <input type="password" {...register("password", { required: true })} />
+                            </div>
+                        }
+                        <input type="submit" value={resetPass ? 'Reset Password' : (registerUser ? 'Login' : 'Register')} />
+                    </form>
+                    <div className='or'>
+                        <div></div>
+                        <p>Or</p>
+                        <div></div>
+                    </div>
+                    <div className='socialLogin'>
+                        {
+                            resetPass || <>
+                                <button onClick={()=>signInWithFacebook()}><Facebook />{registerUser?'Login':'Register'} with Facebook</button>
+                                <button onClick={() => signInWithGoogle()}><Google /> {registerUser?'Login':'Register'} with Google</button>
+                                <button onClick={() => setResetPass(!resetPass)}>Reset Password?</button>
+                            </>
+                        }
+                        <button onClick={registerHandlar}> {registerUser ? 'Create new Account' : 'Already have account Login'}</button>
+                    </div>
+                </div>
+
+            </div>
+        );
 };
 
 export default Login;
