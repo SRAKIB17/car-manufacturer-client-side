b913b5c167fa84eb500f188e442355ebe5ee3df0
SRAKIB17 <rakibulssc5@gmail.com>
2022-05-05 21:33:51 +0600

    done .env

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,25 +5,24 @@ import {getAuth} from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBAmhoxgDUPK_lbiYQslWji9azbnNkQfd4",
-  authDomain: "grocery-warehouse-e95eb.firebaseapp.com",
-  projectId: "grocery-warehouse-e95eb",
-  storageBucket: "grocery-warehouse-e95eb.appspot.com",
-  messagingSenderId: "622234855952",
-  appId: "1:622234855952:web:4dee904ccfd210b4f0f895"
-};
-
-// const ff = {
-//   apiKey:process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBAmhoxgDUPK_lbiYQslWji9azbnNkQfd4",
+//   authDomain: "grocery-warehouse-e95eb.firebaseapp.com",
+//   projectId: "grocery-warehouse-e95eb",
+//   storageBucket: "grocery-warehouse-e95eb.appspot.com",
+//   messagingSenderId: "622234855952",
+//   appId: "1:622234855952:web:4dee904ccfd210b4f0f895"
 // };
 
-// console.log(ff)
+const firebaseConfig = {
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
